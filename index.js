50adc6a73081586c7b53405aa65c29acd7565cb2
Fernando Gomez <fhergomez37@gmail.com>
2017-10-03 05:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ function viewCart() {
 
 function total() {
   // write your code here
+  let t = 0;
+  for (let i =0;i<cart.length;i++) {
+    for (let item in cart[i]) {
+      t+= cart[i][item];
+    }
+  }
+  return t;
 }
 
 function removeFromCart(item) {
