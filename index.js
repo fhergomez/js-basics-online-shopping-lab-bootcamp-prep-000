9c49984979ec39c032576485da4617d578592532
Fernando Gomez <fhergomez37@gmail.com>
2017-10-03 05:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+let cart = [];
 
 function getCart() {
  return cart;
@@ -9,12 +9,35 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  const itemPrice = Math.floor(Math.random()*100) + 1;
+  cart.push({[itemName]:itemPrice});
+  console.log(`${itemName} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  }
+
+  let itemsAndPrices = [];
+  for (let i = 0;i < cart.length;i++) {
+    let itemsAndPrice = cart[i];
+    let item = Object.keys(itemsAndPrice)[0];
+    let price = itemsAndPrice[item];
+    itemsAndPrices.push(`${item} at \$${price}`);
+  }
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+    break;
+    default:
+    itemsAndPrices[cart.length-1] = "and ".concat(itemsAndPrices[l-1]);
+    itemsAndPrices = itemsAndPrices.join(", ");
+  }
 }
 
 function total() {
